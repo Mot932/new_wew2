ec07b9f25e8d6e241cb8ad63f33be63b2815c649
DEB <112969476+Mot932@users.noreply.github.com>
2022-11-15 19:48:46 +0300

    Add files via upload

--- main_new.py
+++ main_new.py
@@ -57,10 +57,24 @@ def make_hero(
         inventory
 ]
 
-p1 = make_hero()
-p2 = make_hero()
-p3 = make_hero(name="Вася",money=0)
 
-print(p1)
-print(p2)
-print(p3)
+def show_hero(hero):
+    print("имя", hero[0])
+    print("здоровье сейчас", hero[1])
+    print("здоровье максимальное", hero[2])
+    print("уровень", hero[3])
+    print("опыт текущий", hero[4])
+    print("опыт до след.уровня", hero[5])
+    print("атака", hero[6])
+    print("защита", hero[7])
+    print("удача", hero[8])
+    print("деньги", hero[9])
+    print("список предмет", hero[10])
+    print(" ")
+
+p1 = make_hero()
+show_hero(p1)
+p2 = make_hero()
+show_hero(p2)
+p3 = make_hero(name="Вася",money=0)
+show_hero(p3)
